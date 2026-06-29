32fbd0e62ce262ae1d44f4aceec05de60045c247
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-06-29 19:12:54 +0700

    chore: sync local repository with remote

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {hostname: "images.unsplash.com"},
+      {hostname: "cdn.dummyjson.com"},
+    ],
+  },
 };
 
 export default nextConfig;
