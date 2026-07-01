2f441ea42da66084a6197adec5f941bcbb956017
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-01 15:55:07 +0700

    feat: creating popular tag component

---
+++ src/components/popular-tag.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Typography from "./ui/typography";
+import Link from "next/link";
+
+const popularTagData = [
+  {name: "Game", link: "game"},
+  {name: "Iphone", link: "iphone"},
+  {name: "TV", link: "tv"},
+  {name: "Asus", link: "asus"},
+  {name: "Macbook", link: "macbook"},
+  {name: "SSD", link: "ssd"},
+  {name: "Powerbank", link: "powerbank"},
+];
+
+const PopularTag = () => {
+  return (
+    <div className="flex flex-col gap-4.5 w-78">
+      <Typography variant={"m"} weight={500} className="text-gray-00 uppercase">
+        popular tag
+      </Typography>
+      <div className="flex items-center gap-2 flex-wrap">
+        {popularTagData.map((item, index) => (
+          <Link
+            key={index}
+            href={item.link}
+            className="flex items-center gap-2 border border-gray-800 hover:bg-gray-800 hover:border-gray-00 hover:rounded-[3px]"
+          >
+            <Typography
+              variant={"s"}
+              weight={500}
+              className="text-gray-400 py-1.5 px-3 hover:text-gray-00"
+            >
+              {item.name}
+            </Typography>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularTag;
