45c706c66fa0b6db57679b95bb53c42dcf028eb5
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-01 14:56:08 +0700

    layouting footer contact detail

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,10 +1,24 @@
+import ContactDetails from "@/components/contact-details";
 import {NewsLetter} from "@/components/news-letter-collection";
+import Typography from "@/components/ui/typography";
+import WrapperMain from "@/components/wrapper-main";
 import React from "react";
 
 const Footer = () => {
+  const fullYear = new Date().getFullYear();
   return (
     <div>
       <NewsLetter />
+      <div className="bg-gray-900 py-18 border-b border-gray-800">
+        <WrapperMain>
+          <ContactDetails />
+        </WrapperMain>
+      </div>
+      <div className="flex items-center justify-center bg-gray-900">
+        <Typography variant={"s"} className="text-gray-300 py-6">
+          Kinbo - eCommerce Template &copy; {fullYear}. Design by Templatecookie
+        </Typography>
+      </div>
     </div>
   );
 };
