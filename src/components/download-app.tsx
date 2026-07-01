77dc66df36048877f3f5edf60605a18ec1e044aa
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-01 15:54:25 +0700

    feat: creating download app componet footer

---
+++ src/components/download-app.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {FaGooglePlay} from "react-icons/fa";
+import {FaApple} from "react-icons/fa";
+import Typography from "./ui/typography";
+
+interface DownloadAppType {
+  icon: React.ReactNode;
+  name: string;
+}
+
+const downloadAppData: DownloadAppType[] = [
+  {icon: <FaGooglePlay size={32} />, name: "Google Play"},
+  {icon: <FaApple size={32} />, name: "Apple Store"},
+];
+
+const DownloadApp = () => {
+  return (
+    <div className="flex flex-col gap-4.5 w-50">
+      <Typography variant={"m"} weight={500} className="text-gray-00 uppercase">
+        download app
+      </Typography>
+      <div className="flex flex-col gap-3">
+        {downloadAppData.map((item) => (
+          <div
+            key={item.name}
+            className="flex items-center justify-center gap-4 w-44 h-17.25 bg-gray-800 rounded-[3px]"
+          >
+            <span className="text-gray-00">{item.icon}</span>
+            <div className="flex flex-col gap-1">
+              <Typography variant={"xs"} className="text-gray-00">
+                Get in now
+              </Typography>
+              <Typography variant={"s"} weight={600} className="text-gray-00">
+                {item.name}
+              </Typography>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DownloadApp;
