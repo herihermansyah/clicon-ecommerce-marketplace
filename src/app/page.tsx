43bc82b4ef500d8473aa84346ca4facde8fc96a6
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-04 20:50:02 +0700

    sync components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import CategorySwiper from "@/components/category-swiper";
 import BestDeals from "@/components/sections/best-deals";
 import HeroSection from "@/components/sections/hero-section";
+import MobileAccessories from "@/components/sections/mobile-accessories";
+import ProductMultiList from "@/components/sections/product-multi-list";
+import PromoBanner from "@/components/sections/promo-banner";
 import React from "react";
 
 function page() {
@@ -9,6 +12,9 @@ function page() {
       <HeroSection />
       <BestDeals />
       <CategorySwiper />
+      <MobileAccessories />
+      <PromoBanner />
+      <ProductMultiList />
     </div>
   );
 }
