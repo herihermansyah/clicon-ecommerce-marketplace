69eb83132a571a7965825f62d62615bb885cec18
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-07 22:31:39 +0700

    add router page track order

---
+++ src/app/track-order/page.tsx
@@ -0,0 +1,12 @@
+import TrackOrderSection from "@/components/sections/track-order-section";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <TrackOrderSection />
+    </div>
+  );
+};
+
+export default page;
